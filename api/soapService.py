665c2765da8820a3ba805d191d647ebea13f9f17
Morningheart <46131687+Morningheart@users.noreply.github.com>
2023-03-10 11:21:50 +0100

    init commit

---
+++ api/soapService.py
@@ -0,0 +1,20 @@
+from spyne import Application, rpc, ServiceBase, Iterable, Integer, Unicode 
+from spyne.protocol.soap import Soap11
+from spyne.server.wsgi import WsgiApplication
+
+class HelloWorldService(ServiceBase):
+    @rpc(Unicode, Integer, _returns=Iterable(Unicode))
+    def say_hello(ctx, name, times):
+        for i in range(times):
+            yield u'Hello, %s' % name
+    
+    @rpc(Integer, Integer, _returns=Integer)
+    def add(ctx, a, b):
+        return a + b
+
+application = Application([HelloWorldService], 'spyne.examples.hello.soap',
+in_protocol=Soap11(validator='lxml'),
+out_protocol=Soap11())
+wsgi_application = WsgiApplication(application)
+
+app = wsgi_application
